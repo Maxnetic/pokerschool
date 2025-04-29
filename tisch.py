c66859f49d4daf4fc0f90c10f4f1fedfd5d833a3
Ovaldeck31 <dominicdelcastillo@outlook.de>
2025-04-29 16:06:18 +0200

    merge Tischkonstruktor

--- tisch.py
+++ tisch.py
@@ -1,17 +1,19 @@
 class Tisch:
 
 
-# Klasse Tisch Methode: Spielererstellung (soll die Spieler-, Rundenanzahl,
-#  das Startvermögen, den Small Blind und den Big Blind implementieren sowie reihum die Spieler ihren Namen eingeben lassen)
-    def __init__(self):
-        self.aktuelle_wette = 0
-        self.ziehstapel = []
-        self.community_cards = []
-        self.rundenanzahl = 1
-        self.spieler = []
-        self.pot = 0
-        self.raise_count = 0
-        self.max_raises = 2
+    def __init__(self):  # Konstruktor für die Tischdaten
+        self.aktuelle_wette = 0 # Aktuelle Wette (der höchste gesetzte Betrag), anfangs bei null
+        self.ziehstapel = [] # Der Kartenvorrat (Deck), anfangs eine leere Liste
+        self.community_cards = [] # Gemeinschaftskarten (Flop, Turn, River), anfangs eine leere Liste
+        self.rundenanzahl = 1 # Anzahl der Runden, die gespielt werden sollen, wird anfangs auf 1 gesetzt, da in einem Spiel mindestens eine Runde gespielt wird, kann aber später noch angepasst werden bei der Tischerstellung
+        self.spieler = [] # Liste der Spieler, anfangs leer
+        self.pot = 0 # Pot (Gesamtbetrag der Einsätze), anfangs bei null
+        self.raise_count = 0 # Zähler für die Anzahl der Erhöhungen in der Runde, anfangs bei null
+        self.max_raises = 2  # Maximale Anzahl von Raises pro Runde, standardmäßig nach Pokerregeln bei 2
+        self.spieleranzahl = int(input("Spieleranzahl: "))  # Eingabe der Anzahl der Spieler
+        self.rundenanzahl = int(input("Rundenanzahl: "))  # Eingabe der Anzahl der Runden
+        self.small_blind = int(input("Small Blind: "))  # Small Blind (Mindestwette)
+        self.big_blind = 2 * self.small_blind   # Big Blind (doppelt so hoch wie Small Blind)
 
     def Spielererstellung(self):
         pass
