15b23886b28f184c20c49ab468b26ec13d30eb6c
Ovaldeck31 <dominicdelcastillo@outlook.de>
2025-05-06 15:42:00 +0200

    Flop, Turn und River

--- tisch.py
+++ tisch.py
@@ -9,13 +9,12 @@ class Tisch:
         self.spieler = []                                   # Liste der Spieler, anfangs leer
         self.pot = 0                                        # Pot (Gesamtbetrag der Einsätze), anfangs bei null
         self.raise_count = 0                                # Zähler für die Anzahl der Erhöhungen in der Runde, anfangs bei null
-        #TODO: verschieben zu Validierung Raise() self.max_raises = 2                                 # Maximale Anzahl von Raises pro Runde, standardmäßig nach Pokerregeln bei 2
-        #TODO: verschieben zu Spielerstellung self.spieleranzahl = int(input("Spieleranzahl: "))  # Eingabe der Anzahl der Spieler
         self.rundenanzahl = int(input("Rundenanzahl: "))    # Eingabe der Anzahl der Runden
         self.small_blind = int(input("Small Blind: "))      # Small Blind (Mindestwette)
         self.big_blind = 2 * self.small_blind               # Big Blind (doppelt so hoch wie Small Blind)
 
     def Spielererstellung(self):
+        self.spieleranzahl = int(input("Spieleranzahl: "))  # Eingabe der Anzahl der Spieler
         #TODO
         pass
 
@@ -28,9 +27,22 @@ class Tisch:
         return(deck)
   
     def Deck_mischen(self, deck):
-        neues_deck = []
+        neues_deck = [] # ein Deck wird in Form einer Liste zufällig gemischt
         for i in range(52):
             pos = random.randint(0, len(deck) - 1)
             neues_deck.append(deck[pos])
             deck.pop(pos)
         return neues_deck
+    
+    def Flop_aufdecken(self):
+        self.gemeinschaftskarten = [self.neues_deck.pop() for _ in range(3)]    # Ziehe 3 Karten für den Flop
+        print(f"Flop: {[str(karte) for karte in self.gemeinschaftskarten]}")    # Zeige die Flop-Karten
+    
+    def Turn_aufdecken(self):
+        self.gemeinschaftskarten.append(self.deck.pop())                        # Ziehe eine Karte für den Turn
+        print(f"Turn: {[str(karte) for karte in self.gemeinschaftskarten]}")    # Zeige die Turn-Karte
+
+    def River_aufdecken(self):
+        self.gemeinschaftskarten.append(self.deck.pop())                        # Ziehe eine Karte für den River
+        print(f"River: {[str(karte) for karte in self.gemeinschaftskarten]}")   # Zeige die River-Karte
+
