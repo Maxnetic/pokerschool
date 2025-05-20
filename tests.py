82d2b32b1e75c190a006c17b00ebe8a928378fc0
Maximilian Göhner <maxgoe119@googlemail.com>
2025-05-20 14:58:21 +0200

    Tests added

--- tests.py
+++ tests.py
@@ -1,11 +1,38 @@
-from spieler import Spieler
+import unittest
 
-def Spielertest():
-    a = Spieler(100, "Mikka")
-    assert a.name == "Mikka"
+class TestTisch(unittest.TestCase):
 
-def test_karte():
-    #assert
-    pass
+    def test_gemischt(self):    #Larissa
+        pass
+    
+    def test_deckerstellt(self): # David
+        pass
 
-Spielertest()
+    def test_gemeinschaftskarten(self): # Dominic
+        pass
+
+    def test_spieler_erstellt(self):    # Mikka
+        pass
+
+    def test_kartenausgeteilt(self): # Lucia
+        pass
+
+    def test_tischerstellt(self): # Moritz
+        pass
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
